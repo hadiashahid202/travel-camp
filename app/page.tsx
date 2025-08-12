9fc4ee48df4ba2963aa9485c55b74e040abe98c3
Hadia Shahid <hadiashahid202@gmail.com>
2025-08-12 21:35:11 +0500

    travel-camp

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/Hero";
-import Camp from "@/components/camp";
+import Camp from "@/components/Camp";
 import Guide from "@/components/Guide";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
